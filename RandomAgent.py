817fb0d7dbd11e05c46c4f65187e35f4c144f58c
Jonas <jonaskorkosh@gmail.com>
2024-05-15 15:32:07 +0100

    rename

--- RandomAgent.py
+++ RandomAgent.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#class not used as for now
 class RandomAgent:
     def __init__(self, name: str):
         self.name = name
@@ -10,3 +11,7 @@ class RandomAgent:
 
     def get_action(self) -> int:
         return np.random.randint(5)
+
+#function to be used in simple_tag.py
+def get_random_action(this_agent, observations):
+    return np.random.randint(5)
